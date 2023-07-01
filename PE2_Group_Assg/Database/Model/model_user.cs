61679f416da80419bd83c2a651bf1c3e0cffe3c3
Ong Cong Kin <congkinong0112@gmail.com>
2023-07-02 00:30:26 +0800

    database

---
+++ PE2_Group_Assg/Database/Model/model_user.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace PE2_Group_Assg.Database.Model
+{
+    public class model_user
+    {
+        public int userId { get; set; }
+        public string first_name { get; set; }
+        public string last_name { get; set; }
+        public bool gender { get; set; } // false = female, true=male
+        public DateTime date_of_birth { get; set; }
+
+
+    }
+}
