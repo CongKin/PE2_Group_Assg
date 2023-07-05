787d426cd5957da6495aa31eb459c332024f5547
Ong Cong Kin <congkinong0112@gmail.com>
2023-07-06 02:05:48 +0800

    login + master page

---
+++ PE2_Group_Assg/WebForm/Login.aspx.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace PE2_Group_Assg.WebForm
+{
+    public partial class Login : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void sign_in_clicked(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
